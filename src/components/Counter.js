8b7c843b8553aac8d2eddff4887b37d06117ee65
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-07-04 16:03:30 +0530

    first-commit

---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import classes from './Counter.module.css';
+import {useSelector,useDispatch} from 'react-redux' ;
+const Counter = () => {
+
+//use selector hook to extract value from redux central store  BASICALLY WE ARE ADDING SUBSCRIPTION PART to component
+ const counter =    useSelector(state=> state.counter)          //in use selector we pass function which recive state managed by our REDUX
+  
+ //use dispatch to dispatch action provided in central store
+ const dispatch = useDispatch();
+
+ const incrementHandler = ()=>{
+    dispatch({type:'increment'})
+ }
+ const decrementHandler =()=>{
+     dispatch({type:'decrement'})
+ }
+ 
+ const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>-- {counter} --</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+
+import {createStore} from 'redux';
+
+//reducer functiom step 2
+const counterReducer  = (state = {counter:0},action)=>{
+  if(action.type ==='increment'){
+    return{
+        counter:state.counter +1
+    }
+  }
+  if(action.type ==='decrement'){
+    return{
+        counter:state.counter -1
+    }
+  };
+  return state;
+}
+//store creation step 1 and in step 3 attach to reducer function (see chart in task 12) 
+const store = createStore(counterReducer);
+
+export default store;
+
+//step 4 now to connect store to all react component we export it
+//video 10 now see index.js file in src folder i.e at topmost component we provide our redux store so to become accessible to all component
